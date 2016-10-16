7fc1cc288c0be86c4a10f6492d4816c3048272b8
Samuel San Nicolas <samuelsan3@gmail.com>
2016-10-16 03:39:19 -0700

    Fix starting backend

--- app.py
+++ app.py
@@ -3,15 +3,21 @@ from twilio.rest import TwilioRestClient
 from twilio import twiml
 from credentials import settings
 import requests
+from flaskext.mysql import MySQL
 
+# Declares flask app
 app = Flask(__name__)
 
+# MySQL setup
+# mysql = MySQL()
+# mysql.init_app(app)
+# cursor = mysql.get_db().cursor()
+
+# For Twilio
 client = TwilioRestClient(settings['sid'], settings['auth'])
-
-
-nums = {}
 twilio_number = '(253) 343-9145'
 
+# Changes Jinja's expression blocks so angular can work
 jinja_options = app.jinja_options.copy()
 jinja_options.update(dict(
     block_start_string='<%',
@@ -23,19 +29,23 @@ jinja_options.update(dict(
 ))
 app.jinja_options = jinja_options
 
+# Main front-end endpoint
 @app.route('/')
 def hello():
 	return render_template('index.html')
 
+# Handle receiving text messages
 @app.route('/api/receive', methods=['POST'])
 def recieve_sms():
 	from_number = requests.values.get('From', None)
-	from_msg = requests.values.get('Body', None)
+	to_number = requests.values.get('To', None)
+	text = requests.values.get('Text', None)
 	nums[from_number].append(from_msg)
 	res = twiml.Response()
 	res.message("Got your message!")
 	return str(res)
 
+# Handle sending back text messages
 @app.route('/api/messages/', methods=['GET', 'POST'])
 def show_messages():
 	if len(nums) == 0:
@@ -43,9 +53,10 @@ def show_messages():
 	else:
 		return flask.jsonify(nums)
 
+# Handles receiving a web message
 @app.route('/api/chat/receive', methods=['GET'])
 def process_message():
-	text = request.args.get('text')
+	text = request.values.get('text')
 	# get the sentiment
 	# Decide how to proceed
 	return jsonify({'text': text})
--- static/app.js
+++ static/app.js
@@ -2,10 +2,25 @@ var app = angular.module('ahunaApp', ['ui.router']);
 
 app.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 
-	$scope.test = [];
+	$scope.conversation = []
+	$scope.newIn = false;
+	$scope.newOut = false;
 
-	$scope.addText = function(text) {
-		$scope.test.push(text);
+	$scope.handleInput = function(text) {
+		$scope.newIn = true;
+		$scope.conversation.push({text: text, author: 'user'});
+		// Uses a GET request for now. Fix later for security.
+		$http({
+			method: 'GET',
+			url: '/api/chat/receive?text=' + text,
+		}).then(function(res) {
+			console.log(res.data);
+			if (res.data.text != null) {
+				$scope.conversation.push({text: res.data.text, author: 'bot'});
+			} else {
+				$scope.conversation.push({text: "I wasn't able to process that. Please try again.", author: 'bot'})
+			}
+		});
 	};
 
 }]);
